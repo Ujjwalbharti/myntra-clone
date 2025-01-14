4d905e6f8b90717cd2d2f8bf69195553d82a2645
Ujjwal Bharti <ujjwalbharti111@gmail.com>
2025-01-15 01:57:14 +0530

    feat: added category on hover feature

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -6,21 +6,46 @@ import Image from 'next/image';
 const SiteHeader = () => {
     return (
         <>
-            <div className='h-[80px] flex items-center justify-center w-full p-6 text-gray-700 font-sans font-bold z-30 fixed bg-white'>
-                <div className='flex w-1/2 justify-start text-sm'>
-                    <div className='mx-8 items-center justify-center'>
+            <div className='h-[80px] flex items-center justify-center w-full text-gray-700 font-sans font-bold z-30 fixed bg-white'>
+                <div className='h-[80px] flex w-1/2 justify-start text-sm ml-6'>
+                    <div className='mx-8 flex items-center justify-center'>
                         <Image src={"/myntra-icon.svg"} width={53} height={36} alt='icon' />
                     </div>
                     <div className='flex items-center justify-center mr-4 uppercase gap-12 ml-4'>
-                        <span className='block'>Men</span>
-                        <span className='block'>Women</span>
-                        <span className='block'>Kids</span>
-                        <span className='block'>Home & Living</span>
-                        <span className='block'>Beauty</span>
+                        <div className='flex items-center h-full relative group hover:border-b-4 hover:border-black'>
+                            <span className='block'>Men</span>
+                            <div className='hidden fixed h-[55%] w-[70%] left-[117px] top-[80px] z-30 bg-white group-hover:block'>
+                                Men
+                            </div>
+                        </div>
+                        <div className='flex items-center h-full relative group hover:border-b-4 hover:border-black'>
+                            <span className='block'>Women</span>
+                            <div className='hidden fixed h-[55%] w-[70%] left-[117px] top-[80px] z-30 bg-white group-hover:block'>
+                                Women
+                            </div>
+                        </div>
+                        <div className='flex items-center h-full relative group hover:border-b-4 hover:border-black'>
+                            <span className='block'>Kids</span>
+                            <div className='hidden fixed h-[55%] w-[70%] left-[117px] top-[80px] z-30 bg-white group-hover:block'>
+                                Kids
+                            </div>
+                        </div>
+                        <div className='flex items-center h-full relative group hover:border-b-4 hover:border-black'>
+                            <span className='block'>Home & Living</span>
+                            <div className='hidden fixed h-[55%] w-[70%] left-[117px] top-[80px] z-30 bg-white group-hover:block'>
+                                Home & Living
+                            </div>
+                        </div>
+                        <div className='flex items-center h-full relative group hover:border-b-4 hover:border-black'>
+                            <span className='block'>Beauty</span>
+                            <div className='hidden fixed h-[55%] w-[60%] left-[117px] top-[80px] z-30 bg-white group-hover:block'>
+                                Beauty
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='flex w-1/2 justify-between'>
-                    <div className='flex justify-center items-center w-[75%] text-gray-600 bg-gray-100 focus-within:bg-white transition focus-within:border'>
+                <div className='h-[80px] flex w-1/2 items-center justify-between mr-6'>
+                    <div className='h-[50%] flex justify-center items-center w-[75%] text-gray-600 bg-gray-100 focus-within:bg-white transition focus-within:border'>
                         <FaSearch className='mr-5 font-normal' />
                         <input type='text' className='w-[88%] bg-transparent outline-none font-normal text-sm font-sans' placeholder='Search for products, brands and more' />
                     </div>
