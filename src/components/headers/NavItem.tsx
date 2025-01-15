50865cd2c0c644e6b187b6b41c1788d95073ca97
Ujjwal Bharti <ujjwalbharti111@gmail.com>
2025-01-15 12:43:44 +0530

    feat: nav item refactor

--- src/components/headers/NavItem.tsx
+++ src/components/headers/NavItem.tsx
@@ -1,30 +1,37 @@
 'use client';
 
 import { useGlobalContext } from "@/context/GlobalContext";
+import React from "react";
 
 interface NavItemProps {
-    type: string
+    type: string;
 }
 
 const NavItem: React.FC<NavItemProps> = ({ type }) => {
-    const context = useGlobalContext();
-    function handleMouseEnter() {
-        context.updateDrawer(true);
-    }
-    function handlerMouseLeave() {
-        context.updateDrawer(false);
-    }
+    const { updateDrawer } = useGlobalContext();
+
+    const handleMouseEnter = React.useCallback(() => {
+        updateDrawer(true);
+    }, [updateDrawer]);
+
+    const handleMouseLeave = React.useCallback(() => {
+        updateDrawer(false);
+    }, [updateDrawer]);
+
     return (
         <div
-            className='flex items-center h-full relative group border-b-4 border-white hover:border-black hover:cursor-pointer'
+            className="flex items-center h-full relative group border-b-4 border-white group-hover:border-black cursor-pointer"
             onMouseEnter={handleMouseEnter}
-            onMouseLeave={handlerMouseLeave}>
-            <span className='block'>{type}</span>
-            <div className='opacity-0 fixed h-[55%] w-[70%] left-[117px] top-[78px] z-30 bg-white group-hover:opacity-100 transition-all duration-500 ease-in-out'>
+            onMouseLeave={handleMouseLeave}
+        >
+            <span className="block">{type}</span>
+            <div
+                className="opacity-0 fixed h-[55%] w-[70%] left-[117px] top-[78px] z-30 bg-white group-hover:opacity-100 transition-all duration-500 ease-in-out"
+            >
                 {type}
             </div>
         </div>
     );
-}
+};
 
 export default NavItem;
